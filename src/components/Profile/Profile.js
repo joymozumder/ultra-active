00b5530264fc922d7d73a0119d657f12d5c028d2
joymozumder <joy.mozumder.1003@gmail.com>
2022-09-29 21:40:23 +0600

    added activity comleted button

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,55 +4,79 @@ import img from '../../images/45297900.jpg'
 
 const Profile = () => {
     return (
-        <div className="col-md-3  p-3 profile">
-            <div className='d-flex  align-items-center '>
-                <img src={ img } alt="" className='rounded-circle img-fluid' id='profile-picture'></img>
-                <div className='ms-3'>
-                    <h5>Joy Mozumder</h5>
-                    <p className='mb-0'>Chattogram</p>
-                </div>
-            </div>
-            <div className='d-flex flex-wrap justify-content-around align-items-center bg-white mt-3 py-3 rounded'>
-                <div className='d-flex flex-column justify-content-center align-items-center'>
-                    <h4><strong>75</strong><sub>kg</sub> </h4>
-                    <p className='m-0'>Weight</p>
-                </div>
-                <div className='d-flex flex-column justify-content-center align-items-center'>
-                    <h4><strong>6.5</strong></h4>
-                    <p className='m-0'>Height</p>
-                </div>
-                <div className='d-flex flex-column justify-content-center align-items-center'>
-                    <h4><strong>25</strong><sub>yrs</sub></h4>
-                    <p className='m-0'>Age</p>
-                </div>
-                
-            </div>
-
-            <div className='mt-4'>
-                <h5 className='mb-2'>Add A Break</h5>
-                <div className='bg-white d-flex p-2 justify-content-around rounded'>
-                    <button className='btn rounded-circle bg-secondary text-white p-2'>10s</button>
-                    <button className='btn rounded-circle bg-secondary text-white p-2'>20s</button>
-                    <button className='btn rounded-circle bg-secondary text-white p-2'>30s</button>
-                    <button className='btn rounded-circle bg-secondary text-white p-2'>40s</button>
-                    <button className='btn rounded-circle bg-secondary text-white p-2'>50s</button>
-                </div>
-            </div>
-
-            <div className='mt-3'>
-                <h5 className='mb-2'>Exercise Details</h5>
-                <div className='d-flex justify-content-between p-3 bg-white rounded mb-2'>
-                    <p className='m-0'>Exercise Time</p>
-                    <p className='m-0'>200 seconds </p>
-                </div>
-                <div className='d-flex justify-content-between p-3 bg-white rounded mb-5'>
-                    <p className='m-0'>Break Time</p>
-                    <p className='m-0'>15 seconds</p>
-                </div>
-                
-            </div>
-
+      <div className="col-md-3  p-3 profile">
+        <div className="d-flex  align-items-center ">
+          <img
+            src={img}
+            alt=""
+            className="rounded-circle img-fluid"
+            id="profile-picture"
+          ></img>
+          <div className="ms-3">
+            <h5>Joy Mozumder</h5>
+            <p className="mb-0">Chattogram</p>
+          </div>
         </div>
+        <div className="d-flex flex-wrap justify-content-around align-items-center bg-white mt-3 py-3 rounded">
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <h4>
+              <strong>75</strong>
+              <sub>kg</sub>{" "}
+            </h4>
+            <p className="m-0">Weight</p>
+          </div>
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <h4>
+              <strong>6.5</strong>
+            </h4>
+            <p className="m-0">Height</p>
+          </div>
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <h4>
+              <strong>25</strong>
+              <sub>yrs</sub>
+            </h4>
+            <p className="m-0">Age</p>
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <h5 className="mb-2">Add A Break</h5>
+          <div className="bg-white d-flex p-2 justify-content-around rounded">
+            <button className="btn rounded-circle bg-secondary text-white p-2">
+              10s
+            </button>
+            <button className="btn rounded-circle bg-secondary text-white p-2">
+              20s
+            </button>
+            <button className="btn rounded-circle bg-secondary text-white p-2">
+              30s
+            </button>
+            <button className="btn rounded-circle bg-secondary text-white p-2">
+              40s
+            </button>
+            <button className="btn rounded-circle bg-secondary text-white p-2">
+              50s
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-3">
+          <h5 className="mb-2">Exercise Details</h5>
+          <div className="d-flex justify-content-between p-3 bg-white rounded mb-2">
+            <p className="m-0">Exercise Time</p>
+            <p className="m-0">200 seconds </p>
+          </div>
+          <div className="d-flex justify-content-between p-3 bg-white rounded mb-5">
+            <p className="m-0">Break Time</p>
+            <p className="m-0">15 seconds</p>
+          </div>
+        </div>
+
+        <div>
+          <button className="btn btn-primary w-100">Activity Completed</button>
+        </div>
+      </div>
     );
 };
 
