a244bccfa8b6e42654336b8525431f078d71affc
joymozumder <joy.mozumder.1003@gmail.com>
2022-09-29 20:33:16 +0600

    fetched exercises data

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Exercises.css'
 const Exercises = () => {
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+      fetch("exercises.json")
+        .then((res) => res.json())
+        .then((data) => setExercises(data));
+    }, []);
+
     return (
-      <div className='exercises'>
-        <h1>Absolute Fitness</h1>
+      <div className="exercises">
+        <h1 className="mb-3">Absolute Fitness</h1>
+        <h5 className="mb-3">Select today’s exercise</h5>
+        <div>
+            <p>{exercises.length}</p>
+        </div>
       </div>
     );
 };
